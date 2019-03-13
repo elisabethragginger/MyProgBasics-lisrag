b7884b9b4d9092cc655d35047448ab2b12ae2298
elisabethragginger <elisabeth.ragginger@htl-salzburg.ac.at>
2019-03-13 17:11:37 +0100

    Update

---
+++ Assets/Scripts/ProgBasicsPartOne.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgBasicsPartOne : MonoBehaviour 
+{
+
+    int index = 0;
+    int step = 5;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        index += step;
+        Debug.Log(index);
+	}
+}
