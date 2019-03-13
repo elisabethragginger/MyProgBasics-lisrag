47fdc5b383251c6064f541e1125e4c30b30d5fba
elisabethragginger <elisabeth.ragginger@htl-salzburg.ac.at>
2019-03-13 17:53:48 +0100

    Update 3

--- Assets/Scripts/ProgBasicsPartOne.cs
+++ Assets/Scripts/ProgBasicsPartOne.cs
@@ -6,7 +6,7 @@ public class ProgBasicsPartOne : MonoBehaviour
 {
 
     int index = 0;
-    int step = 5;
+    int step = 1;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +15,43 @@ public class ProgBasicsPartOne : MonoBehaviour
 	
 	// Update is called once per frame
 	void Update () {
-        index += step;
-        Debug.Log(index);
+        CountUp(3);
+        Text("myStringText");
+        MultNumbers(3.3f, 1.0f);
+        MixTypes("firstTry", 4, 3.9f);
 	}
+
+    private void CountUp() 
+
+    {
+    index = index + step;
+    Debug.Log(index);
+
+    }
+
+    private void CountUp(int myStep)
+
+    {
+        index = index + myStep;
+        Debug.Log(index);
+
+    }
+
+    private void Text(string text)
+    {
+        Debug.Log(text);
+    }
+
+    private void MultNumbers(float first, float second) 
+    {
+        float tmp = first * second;
+        Debug.Log("MultNumbers: " + tmp);
+
+    }
+
+    private void MixTypes(string test, int index2, float end)
+    {
+        Debug.Log(test + " , " + index2 + " , " + end);
+    }
+
 }
